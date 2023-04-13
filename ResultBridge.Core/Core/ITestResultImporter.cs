5d6cbbe3fddba3a01c833e65c79fa733c64b543a
Granzow, Dario <dario.granzow@karlstorz.com>
2023-04-13 18:40:39 +0200

    feature: added new interface

---
+++ ResultBridge.Core/Core/ITestResultImporter.cs
@@ -0,0 +1,10 @@
+﻿using ResultBridge.Core.Model;
+using System.Collections.Generic;
+
+namespace ResultBridge.Core.Core
+{
+    public interface ITestResultImporter
+    {
+        public void SyncResultsToWindchill(IList<TestCase> testResults);
+    }
+}
---
+++ ResultBridge.Core/Core/TestResultImporter.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using ResultBridge.Core.Model;
+
+namespace ResultBridge.Core.Core
+{
+    public class TestResultImporter : ITestResultImporter
+    {
+        public void SyncResultsToWindchill(IList<TestCase> testResults)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
